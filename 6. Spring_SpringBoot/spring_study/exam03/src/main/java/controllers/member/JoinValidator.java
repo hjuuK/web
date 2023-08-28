2f2debfb284f21563208b3c1f40df2c8694b8be2
hjuuK <khj020508@naver.com>
2023-08-28 20:16:25 +0900

    0828 수업

--- 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/JoinValidator.java
+++ 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/JoinValidator.java
@@ -12,7 +12,20 @@ public class JoinValidator implements Validator {
     }
 
     @Override
-    public void validate(Object target, Errors errors) {
+    public void validate(Object target, Errors errors) { // 검증
+        JoinForm joinForm = (JoinForm)target;
 
+        /**
+         * 1. 필수 항목 검증 (userId, userPw, userPwRe, userNm) , null, "", "   "
+         *
+         */
+        String userId = joinForm.getUserId();
+        String userPw = joinForm.getUserPw();
+        String userPwRe = joinForm.getUserPwRe();
+        String userNm = joinForm.getUserNm();
+
+        if (userId == null || userId.isBlank()) {
+            errors.rejectValue("userId", "required", "아이디를 입력하세요.");
+        }
     }
 }
