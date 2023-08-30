d31af03898e3b591720dfac5a876b1a61da4eb6b
hjuuK <khj020508@naver.com>
2023-08-30 20:15:00 +0900

    0830 수업

--- 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
+++ 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
@@ -3,10 +3,7 @@ package controllers.member;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/members")
@@ -18,9 +15,19 @@ public class MembersController {
     }
 
     @GetMapping("/info/{userId}")
-    public String info(@PathVariable String userId) {
+    public String info(@PathVariable("id") String userId) {
         System.out.println("userId : " + userId);
 
+        boolean result = false;
+        if (!result) {
+            throw new RuntimeException("예외 발생...!!!");
+        }
+
         return "member/info";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String errorHandler() {
+        return "error/common";
     }
 }
