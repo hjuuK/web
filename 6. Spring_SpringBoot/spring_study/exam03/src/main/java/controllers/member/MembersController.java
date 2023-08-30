4f22739abfe4338bfd9fc45ff48fecf6e04abbe0
hjuuK <khj020508@naver.com>
2023-08-30 19:51:48 +0900

    0830 수업

--- 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
+++ 6. Spring_SpringBoot/spring_study/exam03/src/main/java/controllers/member/MembersController.java
@@ -1,14 +1,26 @@
 package controllers.member;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/members")
 public class MembersController {
     @GetMapping
-    public String list() {  // 날짜를 통한 회원목록 조회
+    public String list(@ModelAttribute @Valid SearchForm form, Errors errors) {  // 날짜를 통한 회원목록 조회
+
         return "member/list";
+    }
+
+    @GetMapping("/info/{userId}")
+    public String info(@PathVariable String userId) {
+        System.out.println("userId : " + userId);
+
+        return "member/info";
     }
 }
---
+++ 6. Spring_SpringBoot/spring_study/exam03/src/main/webapp/WEB-INF/view/member/info.jsp
@@ -0,0 +1,1 @@
+<%@ contentType="text/html; charset=UTF-8" %>
