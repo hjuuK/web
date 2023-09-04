bab7770d6d69c0e51405fd5173a968fe4654837b
hjuuK <khj020508@naver.com>
2023-09-04 21:52:57 +0900

    0904 수업

---
+++ 6. Spring_SpringBoot/spring_study/exam04/src/test/java/org/koreait/Exam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest	// junit5, mockito, spring-test 의존성 추가 / MockMvc 통합 테스팅? 추가
+class Exam04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
