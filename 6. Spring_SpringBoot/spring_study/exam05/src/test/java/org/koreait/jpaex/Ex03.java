1389e6f601418e3c560184a9ea883d8a450f049e
hjuuK <khj020508@naver.com>
2023-09-06 19:26:31 +0900

    0906 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex03.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex03.java
@@ -2,9 +2,13 @@ package org.koreait.jpaex;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.koreait.entities.Member;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 @Transactional
@@ -12,4 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class Ex03 {
     @PersistenceContext
     private EntityManager em;
+
+    @Test
+    void test1() {
+        Member member = Member.builder()
+                .userId("user01")
+                .userPw("123456")
+                .userNm("사용자01")
+                .regDt(LocalDateTime.now())
+                .build();
+        em.persist(member);
+        em.flush();
+
+        System.out.println(member);
+    }
 }
