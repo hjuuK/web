7fcd6379ca87df1bd1f91ca51c2fa9bc3130be77
hjuuK <khj020508@naver.com>
2023-09-07 20:19:10 +0900

    0907 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -1,7 +1,23 @@
 package org.koreait.repositories;
 
 import org.koreait.entities.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> { // JpaRepository<Entity, 기본키>
+    Member findByUserId(String userId); // 구현체 생성 -> 외부에서 메소드엿던 구현 내용이 바로 추가
+
+    List<Member> findByUserNmContaining(String keyword, Pageable pageAble); // like %키워드%
+
+    List<Member> findByUserNmContainingOrderByUserIdDesc(String keyword); // 내림차순으로 정렬
+
+    List<Member> findByUserNmContainingOrUserIdContainingOrderByUserIdDesc(String keyword1, String keyword2);
+
+    @Query("SELECT m FROM Member m WHERE m.userNm LIKE %:key1% OR m.userId LIKE %:key2% ORDER BY m.userId DESC")
+    List<Member> getList(@Param("key1") String keyword1, @Param("key2") String keyword2);  // JPQL
+
 }
--- 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex04.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex04.java
@@ -1,5 +1,7 @@
 package org.koreait.jpaex;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.constants.Role;
@@ -7,11 +9,17 @@ import org.koreait.entities.Member;
 import org.koreait.repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.Order.desc;  // Sort.by.Order.desc
 
 @SpringBootTest
 @Transactional
@@ -19,6 +27,8 @@ import java.util.List;
 public class Ex04 {
     @Autowired // 의존성 주입
     private MemberRepository repository;
+    @PersistenceContext
+    private EntityManager em;
 
     @BeforeEach
     void init() {
@@ -38,13 +48,52 @@ public class Ex04 {
 
     @Test
     void test1() {
-        List<Member> members = repository.findAll();
+        List<Member> members = repository.findAll();    // 전체 조회
         members.stream().forEach(System.out::println);
     }
 
     @Test
     void test2() {
-        Member member = repository.findById(1L).orElse(null);
+        em.clear(); // 엔티티 제거
+        Member member = repository.findById(1L).orElse(null);   // 기본키로 한개 조회
+
         System.out.println(member);
+
+        repository.delete(member);
+        repository.flush();
+    }
+
+    @Test
+    void test3() {
+        Member member = repository.findByUserId("user2");
+        System.out.println(member);
+    }
+
+    @Test
+    void test4() {
+        //Pageable pageable = PageRequest.of(0, 3); // LIMIT 0, 3 // 3개만 조회 // 1, 2, 3
+        Pageable pageable = PageRequest.of(1, 3,
+                Sort.by(desc("userId"), asc("userNo")));
+                // 또 다시 1부터 조회하면 4, 5, 6 이 조회됨 // 1차는 userId로, 2차는 userNo로 정렬
+        List<Member> members = repository.findByUserNmContaining("용", pageable);
+        members.stream().forEach(System.out::println);
+    }
+
+    @Test
+    void test5() {
+        List<Member> members = repository.findByUserNmContainingOrderByUserIdDesc("용");
+        members.stream().forEach(System.out::println);
+    }
+
+    @Test
+    void test6() {
+        List<Member> members = repository.findByUserNmContainingOrUserIdContainingOrderByUserIdDesc("용", "ser");
+        members.stream().forEach(System.out::println);
+    }
+
+    @Test
+    void test7() {
+        List<Member> members = repository.getList("용", "ser");
+        members.stream().forEach(System.out::println);
     }
 }
