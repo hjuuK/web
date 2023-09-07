c6be4241440cf75fac2e88bbe24d1c538f4fa24d
hjuuK <khj020508@naver.com>
2023-09-07 20:50:38 +0900

    0907 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -1,7 +1,10 @@
 package org.koreait.repositories;
 
+import com.querydsl.core.BooleanBuilder;
 import org.koreait.entities.Member;
+import org.koreait.entities.QMember;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -9,8 +12,9 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> { // JpaRepository<Entity, 기본키>
+import static org.springframework.data.domain.Sort.Order.desc;
 
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> { // JpaRepository<Entity, 기본키>
     Member findByUserId(String userId); // 구현체 생성 -> 외부에서 메소드엿던 구현 내용이 바로 추가
 
     List<Member> findByUserNmContaining(String keyword, Pageable pageAble); // like %키워드%
@@ -22,4 +26,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslP
     @Query("SELECT m FROM Member m WHERE m.userNm LIKE %:key1% OR m.userId LIKE %:key2% ORDER BY m.userId DESC")
     List<Member> getList(@Param("key1") String keyword1, @Param("key2") String keyword2);  // JPQL
 
+    default List<Member> getList2(String keyword1, String keyword2) {
+        QMember member = QMember.member; // 정적 변수를 가져옴 -> 싱글턴 패턴
+        BooleanBuilder builder = new BooleanBuilder(); // and or not 메서드가 정의되어 있음
+        builder.or(member.userNm.contains(keyword1))
+                .or(member.userId.contains(keyword2));
+
+        List<Member> members = (List<Member>)findAll(builder, Sort.by(desc("userId")));
+        return members;
+    }
 }
