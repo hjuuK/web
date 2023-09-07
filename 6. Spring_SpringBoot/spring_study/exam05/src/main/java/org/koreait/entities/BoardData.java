d59ff15979d7f8291e7409eb9213c4ef769d9c56
hjuuK <khj020508@naver.com>
2023-09-07 21:13:58 +0900

    0907 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/BoardData.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/BoardData.java
@@ -19,4 +19,8 @@ public class BoardData extends BaseEntity {
     @Lob
     @Column(nullable = false)
     private String content;
+
+    @ManyToOne // 다대일 관계 / Many: BoardData , One: Member
+    @JoinColumn(name="userNo") // 외래키명 변경 / 기본 외래키명 : 엔티티명_기본키명 ex) member_user_no
+    private Member member;
 }
