bda29fd0947d4425c8dc460f37da66d147dbe044
hjuuK <khj020508@naver.com>
2023-09-11 01:35:47 +0900

    0908 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex07.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/test/java/org/koreait/jpaex/Ex07.java
@@ -1,22 +1,31 @@
 package org.koreait.jpaex;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.koreait.entities.BoardData;
 import org.koreait.entities.HashTag;
 import org.koreait.repositories.BoardDataRepository;
 import org.koreait.repositories.HashTagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class Ex07 {
     @Autowired
     private BoardDataRepository boardDataRepository;
     @Autowired
     private HashTagRepository hashTagRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     @BeforeEach
     void init() {
@@ -41,5 +50,11 @@ public class Ex07 {
         }
 
         boardDataRepository.saveAllAndFlush(items);
+        em.clear();
+    }
+
+    @Test
+    void test1() {
+
     }
 }
