7254d7abbf9ff1fb300dd460a428bc3b6904d8fc
hjuuK <khj020508@naver.com>
2023-09-11 01:24:33 +0900

    0908 수업

--- 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/BoardData.java
+++ 6. Spring_SpringBoot/spring_study/exam05/src/main/java/org/koreait/entities/BoardData.java
@@ -3,6 +3,9 @@ package org.koreait.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
@@ -21,4 +24,7 @@ public class BoardData extends BaseEntity {
     @JoinColumn(name="userNo") // 외래키명 변경 / 기본 외래키명 : 엔티티명_기본키명 ex) member_user_no
     @ToString.Exclude // 순환참조의 무한반복을 방지하기 위한 방법 => toString() 제외됨
     private Member member;
+
+    @ManyToMany
+    private List<HashTag> tags = new ArrayList<>();
 }
