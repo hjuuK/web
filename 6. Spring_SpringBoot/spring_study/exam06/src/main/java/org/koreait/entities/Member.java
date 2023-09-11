b7f494b7c06cf180d081153e3fdb326209d1fd8c
hjuuK <khj020508@naver.com>
2023-09-11 19:51:08 +0900

    0911 수업

--- 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
+++ 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
@@ -9,6 +9,17 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {    // 공통 속성으로 날짜와 시간 자동 추가
+    @Id @GeneratedValue // 자동 증감 번호
+    private Long userNo;
 
+    private String userId;
+
+    private String userPw;
+
+    private String userNm;
+
+    private String email;
+
+    private String mobile;
 }
