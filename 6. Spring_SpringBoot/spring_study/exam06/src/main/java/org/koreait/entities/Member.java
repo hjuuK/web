5f12ee1ddb6f36425ba3ea3dc56cebc7a1fb43a7
hjuuK <khj020508@naver.com>
2023-09-11 20:05:05 +0900

    0911 수업

--- 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
+++ 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/entities/Member.java
@@ -13,13 +13,18 @@ public class Member extends BaseEntity {    // 공통 속성으로 날짜와 시
     @Id @GeneratedValue // 자동 증감 번호
     private Long userNo;
 
+    @Column(length = 40, unique = true, nullable = false) // notnull, unique 제약조건
     private String userId;
 
+    @Column(length = 65, nullable = false)
     private String userPw;
 
+    @Column(length = 40, nullable = false)
     private String userNm;
 
+    @Column(length = 100, nullable = false)
     private String email;
 
+    @Column(length = 11)
     private String mobile;
 }
