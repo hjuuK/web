14c4df371788a034f0366e6ecddd3864c5af768d
hjuuK <khj020508@naver.com>
2023-09-11 20:25:15 +0900

    0911 수업

--- 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/MvcConfig.java
+++ 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/MvcConfig.java
@@ -1,10 +1,23 @@
 package org.koreait.configs;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ResourceBundle;
 
 @Configuration
 @EnableJpaAuditing  // JPA의 변화감지를 감독하고 코드 추가
 public class MvcConfig implements WebMvcConfigurer {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.validation", "messages.errors");
+
+        return ms;
+    }
 }
