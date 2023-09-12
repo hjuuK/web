8f873c34f9ddb457a98b802b2914fe22cb9e0c6d
hjuuK <khj020508@naver.com>
2023-09-12 20:24:35 +0900

    0912 수업

--- 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
+++ 6. Spring_SpringBoot/spring_study/exam06/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -6,11 +6,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {   // localhost:3000 첫 로그인 화면 무력화
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /* 회원 인증 설정 - 로그인 */
+        http.formLogin(f -> {
+           f.loginPage("/member/login")
+                   .usernameParameter("userId")
+                   .passwordParameter("userPw")
+                   .successForwardUrl("/") // 성공하면 이동할 주소
+                   .failureUrl("/member/login"); // 실패하면 이동할 주소
+        });
+
+        /* 회원 로그아웃 설정 */
+        http.logout(f -> {
+            f.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/member/login"); // 로그아웃시 이동할 페이지
+        });
+
         return http.build();
     }
 
