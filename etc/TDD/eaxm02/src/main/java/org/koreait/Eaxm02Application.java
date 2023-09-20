e961f2febdff6f76fdb8820b40367070915d1cdd
hjuuK <khj020508@naver.com>
2023-09-20 20:53:09 +0900

    0920 수업

---
+++ etc/TDD/eaxm02/src/main/java/org/koreait/Eaxm02Application.java
@@ -0,0 +1,13 @@
+package org.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eaxm02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eaxm02Application.class, args);
+	}
+
+}
