c0c05df88743e05fd9b156df4d559fa6efb3109e
hjuuK <khj020508@naver.com>
2023-09-20 21:50:15 +0900

    0920 수업

--- etc/TDD/eaxm02/src/main/java/org/koreait/models/board/BoardDataSaveService.java
+++ etc/TDD/eaxm02/src/main/java/org/koreait/models/board/BoardDataSaveService.java
@@ -2,6 +2,7 @@ package org.koreait.models.board;
 
 import lombok.RequiredArgsConstructor;
 import org.koreait.controllers.board.BoardForm;
+import org.koreait.entities.BoardData;
 import org.koreait.repositories.BoardDataRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,11 @@ public class BoardDataSaveService {
     private final BoardDataRepository repository;
 
     public void save(BoardForm form) {
-
+        BoardData data = BoardData.builder()
+                .poster(form.getPoster())
+                .subject(form.getSubject())
+                .content(form.getContent())
+                .build();
+        repository.saveAndFlush(data);
     }
 }
